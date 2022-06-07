b8e068fc1b6862ef100e583742819dc15482a574
Taisiya <lebei@mail.ru>
2022-06-07 21:47:38 +0300

    Task1 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 7;
+int numberB = 5;
+if(numberA > numberB)
+{
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.WriteLine(numberB);
+}
