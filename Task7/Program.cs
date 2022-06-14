c15f5993130025ca1526adf34aba370e1452c2a6
Taisiya <lebei@mail.ru>
2022-06-14 20:35:01 +0300

    Task 1. Hometask #2 done.

---
+++ Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿int Number = new Random().Next(100,1000);
+Console.WriteLine(Number);
+int NumberA = Number / 100;
+int NumberB = Number / 10;
+int NumberC = NumberB%10;
+int NumberD = NumberC%10;
+
+
+Console.WriteLine(NumberD);
+
+
