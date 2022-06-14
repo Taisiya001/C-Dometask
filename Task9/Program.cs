085daae645c8aa332cafacb90a043564a98c9a6d
Taisiya <lebei@mail.ru>
2022-06-14 21:11:20 +0300

    Task 3. Hometask #2 done

---
+++ Task9/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number == 7)
+{
+    Console.WriteLine("Да");
+}
+if (Number == 6)
+{
+    Console.WriteLine("Да");
+}
+if (Number == 1)
+{
+    Console.WriteLine("Нет");
+}
+if (Number == 2)
+{
+    Console.WriteLine("Нет");
+}
+if (Number == 3)
+{
+    Console.WriteLine("Нет");
+}
+if (Number == 4)
+{
+    Console.WriteLine("Нет");
+}
+if (Number == 5)
+{
+    Console.WriteLine("Нет");
+}
