cf31cbd76a352619841e9eaff1deab055374eb66
Taisiya <lebei@mail.ru>
2022-06-14 20:56:34 +0300

    Task2. Hometask #2 done

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if(Number > 99)
+{
+    Console.WriteLine((Number / 10)%10);
+}
+if(Number < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
