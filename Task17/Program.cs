f30a229abb3f7dd9489ee61b2b0102b14d733f1e
Taisiya <lebei@mail.ru>
2022-06-17 20:40:35 +0300

    Task #3. Hometask #3 done.

---
+++ Task17/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+void Method(int N)
+{
+    for(int i = 0; i <= N ; i++)
+    {
+        int s = i*i*i;
+        Console.WriteLine($"{s}");
+    }
+}
+Method(Number);
+
