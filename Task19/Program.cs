143c27632ed652fab0f9fc1fef6e697e8f00826e
Taisiya <lebei@mail.ru>
2022-06-17 21:05:47 +0300

    Task #2. Hometask #4 done

---
+++ Task19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int Method(int N) 
+{
+    int result = 0;
+    for(int i = 0; i <= N ; i++)
+    {
+        result = result + N%10;
+        N = N/10;
+    }
+    return result;
+}
+Console.WriteLine(Method(Number));
