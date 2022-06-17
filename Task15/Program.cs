bfeb6a9e16ff2a859156f37cf063dc8ddb3885d1
Taisiya <lebei@mail.ru>
2022-06-17 20:17:24 +0300

    Task #1. Hometask #3 done.

---
+++ Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+void Number1(int N)
+{
+    int N1 = N%10;
+    int N2 = N/10000;
+    int N3 = (N/10)%10;
+    int N4 = (N/1000)%10;
+    if(N1 == N2 && N3 == N4)
+    {
+        Console.WriteLine("Это палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+Number1(Number);
