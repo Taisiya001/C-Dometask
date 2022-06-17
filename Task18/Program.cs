1748634dc8e27bf52df56aa8f36a70d66a662cfa
Taisiya <lebei@mail.ru>
2022-06-17 20:51:16 +0300

    Task #1. Hometask #4 done

---
+++ Task18/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+void Method(int N, int B)
+{
+    double d = Math.Pow(N,B);
+    Console.WriteLine($"{d}");
+}
+Method(NumberA,NumberB);
