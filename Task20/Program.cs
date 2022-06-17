b1c8bcf7bf58ae7ffa4949f87f5c49b504fdd89e
Taisiya <lebei@mail.ru>
2022-06-17 21:16:54 +0300

    Task #3.Hometask #4 done.

---
+++ Task20/Program.cs
@@ -0,0 +1,19 @@
+﻿void FillArray(int[] array)
+{
+    int lenght = array.Length;
+    for(int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(0,10);
+    }
+}
+void PrintArray(int[] array)
+{
+    int lenght = array.Length;
+    for(int i = 0; i < lenght;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
