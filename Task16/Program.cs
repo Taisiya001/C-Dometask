26eb8d30e37d4efa9906e21c341f5a0c369b908b
Taisiya <lebei@mail.ru>
2022-06-17 20:33:10 +0300

    Task #2. Hometask 3 done

---
+++ Task16/Program.cs
@@ -0,0 +1,7 @@
+﻿void Distance(int x, int y, int z, int x1, int y1, int z1)
+{
+    double d = Math.Pow(x1-x,2) + Math.Pow(y1-y,2) + Math.Pow(z1-z,2);
+    double sqr = Math.Sqrt(d);
+    Console.WriteLine(sqr);
+}
+Distance (3,2,6,1,8,-7);
