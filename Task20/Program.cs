edc55300314d8522ce7ee691358960fbc1599a3a
Taisiya <lebei@mail.ru>
2022-06-19 18:31:23 +0300

    Task #3.Homework #4 correct

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -1,19 +1,28 @@
-﻿void FillArray(int[] array)
-{
-    int lenght = array.Length;
-    for(int i = 0; i < lenght; i++)
-    {
-        array[i] = new Random().Next(0,10);
-    }
-}
+﻿Console.WriteLine("Введите число");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int Number4 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int Number5 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int Number6 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int Number7 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int Number8 = Convert.ToInt32(Console.ReadLine());
+int [] arr = {Number1, Number2, Number3, Number4, Number5, Number6, Number7, Number8};
+
 void PrintArray(int[] array)
 {
-    int lenght = array.Length;
-    for(int i = 0; i < lenght;i++)
+   int count = array.Length;
+   for(int i = 0; i < count; i++)
     {
-        Console.Write($"{array[i]} ");
+       Console.Write($"{array[i]} ");
     }
+   Console.WriteLine();
 }
-int[] array = new int[8];
-FillArray(array);
-PrintArray(array);
+PrintArray(arr);
