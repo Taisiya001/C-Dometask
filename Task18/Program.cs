88d1575d3908b013a28659e303d14a8ee28586ef
Taisiya <lebei@mail.ru>
2022-06-19 20:02:34 +0300

    Task #1. Homework #4 correct

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -4,14 +4,11 @@ Console.WriteLine("Введите число");
 int NumberB = Convert.ToInt32(Console.ReadLine());
 int Method(int N, int B)
 {
-    int s = 0;
     int r = 1;
     for(int i = 1; i <= B ; i++)
     {
-        s = r*N;
-        r = s; 
-        Console.WriteLine(s);
+        r = r*N;
     }
-    return s;
+    return r;
 }
-Method(NumberA,NumberB);
+Console.WriteLine(Method(NumberA,NumberB));
