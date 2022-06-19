c47d8d285321d7c57de1598615249d5ad960ce8a
Taisiya <lebei@mail.ru>
2022-06-19 18:57:19 +0300

    Task #1.Homework #4 correct

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -2,9 +2,16 @@
 int NumberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число");
 int NumberB = Convert.ToInt32(Console.ReadLine());
-void Method(int N, int B)
+int Method(int N, int B)
 {
-    double d = Math.Pow(N,B);
-    Console.WriteLine($"{d}");
+    int s = 0;
+    int r = 1;
+    for(int i = 1; i <= B ; i++)
+    {
+        s = r*N;
+        r = s; 
+        Console.WriteLine(s);
+    }
+    return s;
 }
 Method(NumberA,NumberB);
