a9fed12af9b589757a03e30829f7301e0ec12b19
Taisiya <lebei@mail.ru>
2022-06-22 20:40:03 +0300

    Task #1.Hometask # 5 done

---
+++ Task25/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] fillArray()
+{
+    int[] array = new int[12];
+    for(int i=0;i < array.Length;i++)
+    {
+        array[i] =new Random().Next(0,10);
+    }
+    return array;
+}
+int[] arr = fillArray();
+
+void ArraySum(int[] array)
+{
+    for(int i = 0; i < array.Length;i++ )
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ArraySum(arr);
+
+void SumPlusAndMinus(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length;i++)
+    {
+        int element = arr[i];
+        if(element % 2 == 0)
+        {
+            count = count + 1;
+        }  
+    }
+    System.Console.WriteLine($"{count} ");
+}
+SumPlusAndMinus(arr);
