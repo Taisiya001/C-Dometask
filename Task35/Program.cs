ec1214f0cd240072563aa56e106b3733bf655a5e
Taisiya <lebei@mail.ru>
2022-06-27 21:40:37 +0300

    Task 41 done.Count.

---
+++ Task35/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] fillArray()
+{
+    int[] array = new int[12];
+    for(int i=0;i < array.Length;i++)
+    {
+        array[i] =new Random().Next(-1000,1000);
+    }
+    return array;
+}
+int[] arr = fillArray();
+
+void ArraySum(int[] array)
+{
+    for(int i = 0; i < array.Length;i++ )
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ArraySum(arr);
+
+void MaxMin(int[] arr)
+{;
+    int count = 0;
+    for(int i = 0; i < arr.Length;i++)
+    {
+        if(arr[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    System.Console.Write($"{count}");
+}
+MaxMin(arr);
