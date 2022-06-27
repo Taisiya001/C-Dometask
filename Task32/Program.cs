1477f96ddeeea19c5477828aa076f8bcca245670
Taisiya <lebei@mail.ru>
2022-06-27 21:37:23 +0300

    Task seminar done

---
+++ Task32/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] fillArray()
+{
+    int[] array = new int[10];
+     for(int i=0;i < array.Length;i++)
+    {
+        array[i] =new Random().Next(0,100);
+    }
+    return array;
+}
+int[] arr = fillArray();
+
+void ArrayShow(int[] array)
+{
+    for(int i = 0; i < array.Length;i++ )
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Fibonach(int[] array)
+{
+    for(int i= 2;i < array.Length;i++)
+    {
+        array[i] = array[i-1] + array[i-2];   
+    }
+}
+ArrayShow(arr);
+Fibonach(arr);
