9f854bc25bf9a07071ef6443f60a093b4e6085e2
Taisiya <lebei@mail.ru>
2022-07-03 12:05:48 +0300

    Task #47. Homework #7 done

---
+++ Task36/Program.cs
@@ -0,0 +1,31 @@
+﻿int [,] CreateArray (int n, int m)
+{
+    return new int [n,m];
+}
+
+void  FillArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintArray(int [,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+int[,] arr = CreateArray(3, 4);
+FillArray(arr);
+PrintArray(arr);
