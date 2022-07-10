ac8506552027412dbaa39d77e8f64ebe1782d149
Taisiya <lebei@mail.ru>
2022-07-10 10:40:07 +0300

    Task #68.Homework #9 done.

---
+++ Task50/Program.cs
@@ -0,0 +1,15 @@
+﻿int Akkerman(int N, int M)
+{
+    if(N == 0)
+    {
+        return M + 1;
+    }
+    if(M == 0 && N > 0)
+    {
+        return Akkerman(N-1,1);
+    }
+    return Akkerman(N-1,Akkerman(N,M-1));
+}
+int result = Akkerman(1,3);
+
+Console.WriteLine(result);
